8519475b0e95ca306c3ac2eff3ffc04e09a8dca5
Tomasz Jakut <vepomoc@gmail.com>
2017-08-18 21:09:54 +0200

    feat(lib): add support for non-ticket branches and PR's descriptions

--- tests/lib.js
+++ tests/lib.js
@@ -12,7 +12,45 @@ describe( 'library methods', () => {
 
 	describe( 'getParentIssue', () => {
 		it( 'returns issue number from ticket branch name', () => {
-			expect( getParentIssue( 't/6785b' ) ).to.equal( '6785' );
+			const inputs = {
+				't/6785b': '6785',
+				't/6660': '6660',
+				't/156-alpha': '156',
+				't/10-alpha-4': '10',
+				'bet/134': null,
+				'hotfix-not': null
+			};
+
+			Object.keys( inputs ).forEach( ( input ) => {
+				expect( getParentIssue( input, '' ) ).to.equal( inputs[ input ] );
+			} );
+		} );
+
+		it( 'returns issue number from PR description', () => {
+			const inputs = {
+				'closes #536': '536',
+				'close #1356': '1356',
+				'closed #4567': '4567',
+				'fix #657': '657',
+				'fixes #999': '999',
+				'fixed #543': '543',
+				'resolve #214': '214',
+				'resolves #333': '333',
+				'resolved #111': '111',
+				'whatever\n\tresolve #212\n': '212',
+				'fixes #456\twhatever': '456',
+				'Closes #587': '587',
+				'closes #4567 test 123': '4567',
+				'closes': null,
+				'closes issue #444': null,
+				'#222': null,
+				'cl ose #25': null,
+				'closes 444': null
+			};
+
+			Object.keys( inputs ).forEach( ( input ) => {
+				expect( getParentIssue( '', input ) ).to.equal( inputs[ input ] );
+			} );
 		} );
 	} );
 
