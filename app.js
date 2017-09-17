3e6ced58f40f6ebffff3bd105586d40d9e4644ea
Tomasz Jakut <vepomoc@gmail.com>
2017-09-17 22:31:43 +0200

    refactor(app): move GitHub API out of lib

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const http = require( 'http' );
 const createHandler = require( 'github-webhook-handler' );
 const { handlePR } = require( './lib/lib' );
+const GitHubAPI = require( 'github' );
+
+const github = new GitHubAPI( {
+	debug: false,
+	headers: {
+		'user-agent': 'Milestone-Checker'
+	},
+	Promise
+} );
+
+github.authenticate( {
+	type: 'token',
+	token: process.env.GH_MSWH_TOKEN
+} );
+
 const handler = createHandler( {
 	path: '/webhook',
 	secret: process.env.GH_MSWH_SECRET
@@ -17,4 +32,6 @@ handler.on( 'error', ( err ) => {
 	console.error( 'Error:', err.message ); // eslint-disable-line no-console
 } );
 
-handler.on( 'pull_request', handlePR );
+handler.on( 'pull_request', ( evt ) => {
+	handlePR( github, evt );
+} );
